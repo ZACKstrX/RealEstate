2bc7e08c724956d9dfa7682a234e2d398b59f143
ZACKstrX <zakimatozi@gmail.com>
2025-01-23 16:37:37 +0100

    create the navbar of the signup page

--- resources/views/SignUp.blade.php
+++ resources/views/SignUp.blade.php
@@ -20,6 +20,35 @@
 
 <body class="bg-parchment">
     
+    <nav class="nav navbar navbar-expand-lg red" data-bs-theme="dark">
+        <div class="container-fluid">
+            <div><img class="logo-nav" src="./images/logo.png" alt=""><a class="navbar-brand"
+                    href="#"></a></div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+
+                        <a class="nav-link green-text" href="#"><strong class="wordhover card-text">Homes</strong></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link green-text " href="#"><strong class="wordhover card-text">Lands</strong></a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><button type="button"
+                                class="mybtn btn bg-parchment "><strong class="wordhover red-text">Sign
+                                    In</strong></button></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
 </body>
 
