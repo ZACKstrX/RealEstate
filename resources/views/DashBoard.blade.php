f2bab487a823374cfbc2c36b8d0c760a396c2a8b
ZACKstrX <zakimatozi@gmail.com>
2025-01-25 15:16:45 +0100

    creating sidebar

--- resources/views/DashBoard.blade.php
+++ resources/views/DashBoard.blade.php
@@ -11,27 +11,82 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker.min.css"
-        rel="stylesheet">
+    
     <link rel="icon" href="./images/logo.png" type="image/png">
-    <link href="./app.css" rel="stylesheet"><link href="./SignUp.css" rel="stylesheet"><link href="./SignIn.css" rel="stylesheet">
+    <link href="./app.css" rel="stylesheet">
+    <link href="./SignUp.css" rel="stylesheet">
+    <link href="./SignIn.css" rel="stylesheet">
+    <link href="./dash.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 </head>
 
-<body style="background-color: #bfdeac;">
-    <div class="header-container">
+<body class="bg-parchment">
 
-        <nav class="nav navbar navbar-expand-lg bg-parchment" data-bs-theme="dark">
-            <div class="container-fluid signav d-flex justify-content-center">
-                <a class="navbar-brand" href="#">
-                    <img class="logo-nav" src="./images/logo.png" alt="Logo">
-                </a>
+
+    <div class="sidebar">
+
+        <div class="top">
+            <div class="logo">
+                <i class="bx bxl-codepen"></i>
+                <span>GreenLeaf </span>
             </div>
-        </nav>
-        <!--navbar end-->
-        
+            <i class="bx bx-menu" id="btn"></i>
+        </div>
 
+        <div class="user">
+            <img src="./images/SignUp.jpg" alt="me" class="user-img">
+            <div><p class="bold">Client B.</p>
+            <p>Admin</p>
+        </div>
+        </div>
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="bx bxs-grid-alt"></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>  <li>
+                <a href="#">
+                    <i class="bx bxs-shopping-bag"></i>
+                    <span class="nav-item">Products</span>
+                </a>
+                <span class="tooltip">Products</span>
+            </li>  <li>
+                <a href="#">
+                    <i class="bx bx-cog"></i>
+                    <span class="nav-item">Settings</span>
+                </a>
+                <span class="tooltip">Settings </span>
+            </li>  <li>
+                <a href="#">
+                    <i class="bx bx-list-check"></i>
+                    <span class="nav-item">Categories</span>
+                </a>
+                <span class="tooltip">Categories</span>
+            </li> <li>
+                <a href="#">
+                    <i class="bx bx-log-out"></i>
+                    <span class="nav-item">Logout</span>
+                </a>
+                <span class="tooltip">Logout</span>
+            </li>
+        </ul>
+    </div>
+
+    <div class="main-content">
+        <div class="container">
+            <h1>Code Commerce </h1>
+        </div>
+    </div>  
 
 </body>
-
+<script>
+    let btn = document.querySelector('#btn');
+    let sidebar = document.querySelector('.sidebar');
+    btn.onclick = function(){
+        sidebar.classList.toggle('active');
+    };
+</script>
 </html>
