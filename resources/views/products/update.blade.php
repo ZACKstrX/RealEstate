155daa8701622ffcd54fab7591b22681924f280e
ZACKstrX <zakimatozi@gmail.com>
2025-02-04 18:56:39 +0100

    all the data that are coming from the bien table are appearing in the inputs of the updating form

--- resources/views/products/update.blade.php
+++ resources/views/products/update.blade.php
@@ -12,29 +12,31 @@
 
                 <div class="form-group col-md-6">
                     <label for="inputPassword4">Title</label>
-                    <input type="text" class="form-control" placeholder="title" name="title">
+                    <input type="text" class="form-control" placeholder="title" name="title" value={{$previous->title}}>
                 </div>
 
                 <div class="form-group">
                     <label for="exampleFormControlTextarea1">Description</label>
-                    <textarea class="form-control" id="inputTittle" name="description" ></textarea>
+                    <textarea class="form-control" id="inputTittle" name="description">{{$previous->description}}</textarea>
                 </div>
                 <div class="mb-3">
                     <label for="formFile" class="form-label">Picture</label>
-                    <input class="form-control" type="file" id="formFile" name="image">
+                    <input class="form-control" type="file" id="formFile" name="image" >
                 </div>
 
                 <div class="row">
                     <div class="form-group col-md-6">
                         <label for="inputEmail4">Phone Number</label>
                         <input type="tel" class="form-control" placeholder="Phone Number" name="phone_number"
-                            pattern="[0-9]{10}" maxlength="10" title="Enter a valid 10-digit phone number">
+                            pattern="[0-9]{10}" maxlength="10" title="Enter a valid 10-digit phone number" 
+                            value={{$previous->phone_number}}>
                     </div>
                     
 
                     <div class="form-group col-md-6">
                         <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" name="email" placeholder="exemple@exemple.com">
+                        <input type="email" class="form-control" name="email" placeholder="exemple@exemple.com"
+                        value={{$previous->email}}>
                     </div>
                 </div>
                 <div class="row">
@@ -42,7 +44,7 @@
                     <div class="form-group col-md-6 col">
                         <label for="inpuPrice">Price</label>
                         <div class="input-group">
-                            <input type="number" class="form-control" name="prix" placeholder="">
+                            <input type="number" class="form-control" name="prix" placeholder="" value={{$previous->prix}}>
                             <span class="input-group-text">MAD</span>
                         </div>
                     </div>
@@ -50,7 +52,7 @@
                     <div class="form-group col">
                         <label for="inputAddress">Surface</label>
                         <div class="input-group">
-                            <input type="number" class="form-control" name="surface" placeholder="">
+                            <input type="number" class="form-control" name="surface" placeholder="" value={{$previous->surface}}>
                             <span class="input-group-text">m²</span>
                         </div>
                     </div>
@@ -62,10 +64,12 @@
                 <div class="row"> {{-- Here --}}
                     <div class="col">
                         <label for="inputCity">City</label>
-                        <select class="form-control" name="city_id">
-                            <option selected>Choose...</option>
+                        <select class="form-control" name="city_id" >
+                            <option >Choose...</option >
                             @foreach ($cities as $city)
-                                <option value="{{ $city->id }}">{{ $city->name }}</option>
+                                <option value="{{ $city->id }}"{{ $previous->city_id == $city->id ? 'selected' : ''}}>
+                                    {{$city->name}}
+                                </option>
                             @endforeach
                         </select>
                     </div>
@@ -75,7 +79,9 @@
                         <select class="form-control" name="type_bien_id">
                             <option selected>Choose...</option>
                             @foreach ($types as $type)
-                                <option value="{{ $type->id }}">{{ $type->name }}</option>
+                                <option value="{{ $type->id }}"{{ $previous->type_bien_id == $type->id ? 'selected' : '' }}>
+                                    {{$type->name}}
+                                </option>
                             @endforeach
                         </select>
                     </div>
@@ -87,7 +93,9 @@
                         <select class="form-control" name="status_id">
                             <option selected>Choose...</option>
                             @foreach ($statuses as $status)
-                                <option value="{{ $status->id }}">{{ $status->name }}</option>
+                                <option value="{{ $status->id }}"{{ $previous->status_id == $status->id ?'selected' : '' }}>
+                                    {{$status->name}}
+                                </option>
                             @endforeach
                         </select>
                     </div>
@@ -96,12 +104,11 @@
                         <label for="inputEtat">Etat</label>
                         <select class="form-control" name="etat_id">
                             <option selected>Choose...</option>
-                            <option value="1">New</option>
-                            <option value="2">Old</option>
+                            <option value="1"{{$previous->etat_id == 1 ? 'selected': ''}}>New</option>
+                            <option value="2" {{$previous->etat_id == 2 ? 'selected' : ''}}>Old</option>
                         </select>
                     </div>
                 </div>
-
                 <div class="row mt-3">
                     <div class="col">
                         <label for="inputBaths">Number of rooms</label>
