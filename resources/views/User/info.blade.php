462297fff8d27e6ffb021911a4a6a5764bb4d8b0
ZACKstrX <zakimatozi@gmail.com>
2025-02-09 18:21:44 +0100

    user info are placed in the inputs to make it easy to be replaced

--- resources/views/User/info.blade.php
+++ resources/views/User/info.blade.php
@@ -1,6 +1,6 @@
 @include('template.nav')
-<div class="img1">
 
+<div class="img1">
     <form class="form1" method="POST" action="/signup">
         @csrf
        
@@ -25,14 +25,14 @@
                 <label for="inputFirstName"><b class="white-text">First Name</b> </label>
                 <input type="text"
                     class="form-control searchopacity1 @error('first_name') is-invalid @enderror"id="inputFirstName"
-                    name="first_name" placeholder="First name">
+                    name="first_name" placeholder="First name" value="{{ auth()->user()->first_name }}">
 
             </div>
             <div class="col">
                 <label for="inputLastName"><b class="white-text">Last Name</b></label>
                 <input type="text"
                     class="form-control searchopacity1 @error('last_name') is-invalid @enderror"id="inputLastName"
-                    name="last_name" placeholder="Last name">
+                    name="last_name" placeholder="Last name" value="{{ auth()->user()->last_name }}">
 
             </div>
 
@@ -41,7 +41,7 @@
             <div class="form-group col-md-6">
                 <label for="inputEmail4"><b class="white-text">Email</b></label>
                 <input type="email" class="form-control searchopacity1 @error('email') is-invalid @enderror"
-                    id="inputEmail4" name="email" placeholder="Email">
+                    id="inputEmail4" name="email" placeholder="Email" value="{{ auth()->user()->email }}">
 
             </div>
 
@@ -49,7 +49,7 @@
                 <label for="inputPhoneNumber"><b class="white-text">Phone Number</b></label>
                 <input type="tel" class="form-control searchopacity1 @error('phone_number') is-invalid @enderror"
                     id="inputPhoneNumber" name="phone_number" placeholder="Phone Number" pattern="[0-9]{10}"
-                    maxlength="10" title="Enter a valid 10-digit phone number">
+                    maxlength="10" title="Enter a valid 10-digit phone number" value="{{ auth()->user()->phone_number }}">
 
             </div>
 
@@ -57,7 +57,7 @@
         <div class="form-group  mb-4">
             <label for="inputAddress "><b class="white-text">Address</b></label>
             <input type="text" class="form-control searchopacity1" id="inputAddress" name="adresse"
-                placeholder="1234 Main St">
+                placeholder="1234 Main St" value="{{ auth()->user()->adresse }}">
         </div>
 
 
@@ -71,7 +71,8 @@
 
             <div class="form-group col-md-6">
                 <label for="birthday"><b class="white-text ">Birthday</b></label>
-                <input name="birthday" id="birthday" class="form-control searchopacity1" placeholder="Pick a Date">
+                <input name="birthday" id="birthday" class="form-control searchopacity1" placeholder="Pick a Date"
+                value="{{ auth()->user()->birthday }}">
 
             </div>
 
@@ -81,9 +82,6 @@
             <button type="submit" class="btn btn-success b"><strong
                     class="wordhover card-text">Update</strong></button>
         </div>
-
-
-
     </form>
 </div>
 </div>
