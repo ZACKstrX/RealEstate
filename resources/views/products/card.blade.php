eefb87f4acdb473edd384f3ef07fcc34ba97aa16
ZACKstrX <zakimatozi@gmail.com>
2025-02-24 17:17:33 +0000

    adding the title and description place

--- resources/views/products/card.blade.php
+++ resources/views/products/card.blade.php
@@ -2,17 +2,24 @@
 <div class="container d-flex col justify-content-center">
 
     <div class="contain">
-        <div class="hh d-flex justify-content-center" >
+        <div class="hh d-flex flex-column " >
             <div class="picture">
 
             </div>
             
+            <div>
+                <h1 class="txt">{{$product->title}}</h1>
+                    <div class="txtCadr">
+                        <p class="txt1"> 
+                            {{$product->description}}
+                        </p>
+                    </div>
+            </div>
         </div>
     </div>
     
     <div class="contain">
         <div class="hh">
-
-
+           
     </div>
 </div>
--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -10,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
