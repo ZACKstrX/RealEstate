7a5babfd122cbaf72a505dbd3ad428823339caeb
ZACKstrX <zakimatozi@gmail.com>
2025-03-02 20:21:35 +0000

    icons are setted

--- resources/views/products/card.blade.php
+++ resources/views/products/card.blade.php
@@ -2,30 +2,115 @@
 <div class="container d-flex col justify-content-center">
 
     <div class="contain">
-        <div class="hh d-flex flex-column " >
+        <div class="hh d-flex flex-column ">
             <div class="picture">
 
             </div>
-            
+
             <div>
-                <h1 class="txt">{{$product->title}}</h1>
-                    <div class="txtCadr">
-                        <p class="txt1"> 
-                            {{$product->description}}
-                        </p>
-                    </div>
+                <h1 class="txt">{{ $product->title }}</h1>
+                <div class="txtCadr">
+                    <p class="txt1">
+                        {{ $product->description }}
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-    
+
     <div class="contain">
         <div class="hh">
-           <div class="IconArea d-flex align-items-center justify-content-center">
-            <i class='bx bxs-bath'></i>
-          </div>
+            <div class="iconsrow d-flex justify-content-around">
 
-           <div class="TextArea">
-            
-           </div>
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="iconsrow d-flex justify-content-around">
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="iconsrow d-flex justify-content-around">
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+                <div class="d-flex">
+                    <div class="IconArea d-flex align-items-center justify-content-center">
+                        <i class='bx bxs-bath'></i>
+                    </div>
+                    <div class="TextArea">
+                        <p>hi</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-</div>
